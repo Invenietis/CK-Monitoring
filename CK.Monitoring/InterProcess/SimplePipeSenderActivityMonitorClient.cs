7519fc481c666bd2b8ccec3dce1c8ff5415bfa90
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-12-14 18:57:10 +0100

    Added AnonymousPipe client/receiver.

---
+++ CK.Monitoring/InterProcess/SimplePipeSenderActivityMonitorClient.cs
@@ -0,0 +1,72 @@
+using CK.Core;
+using CK.Monitoring;
+using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Text;
+
+namespace CK.Monitoring.InterProcess
+{
+    /// <summary>
+    /// Simple activity monitor client that uses a <see cref="AnonymousPipeClientStream"/> to sends log
+    /// entries to a <see cref="SimpleLogPipeReceiver"/>.
+    /// </summary>
+    public class SimplePipeSenderActivityMonitorClient : IActivityMonitorClient, IDisposable
+    {
+        readonly CKBinaryWriter _writer;
+        readonly AnonymousPipeClientStream _client;
+        bool _disposed;
+
+        /// <summary>
+        /// Initializes a new <see cref="SimplePipeSenderActivityMonitorClient"/>.
+        /// </summary>
+        /// <param name="pipeHandlerName">The name of the server pipe.</param>
+        public SimplePipeSenderActivityMonitorClient( string pipeHandlerName )
+        {
+            _client = new AnonymousPipeClientStream( PipeDirection.Out, pipeHandlerName );
+            _writer = new CKBinaryWriter( _client );
+            _writer.Write( LogReader.CurrentStreamVersion );
+        }
+
+        /// <summary>
+        /// Sends a goodbye message (a zero byte) and closes this side of the pipe.
+        /// </summary>
+        public void Dispose()
+        {
+            if( !_disposed )
+            {
+                _disposed = true;
+                _client.WriteByte( 0 );
+                _writer.Dispose();
+                _client.Dispose();
+            }
+        }
+
+        void IActivityMonitorClient.OnAutoTagsChanged( CKTrait newTrait )
+        {
+        }
+
+        void IActivityMonitorClient.OnGroupClosed( IActivityLogGroup group, IReadOnlyList<ActivityLogGroupConclusion> conclusions )
+        {
+            LogEntry.WriteCloseGroup( _writer, group.GroupLevel, group.CloseLogTime, conclusions );
+        }
+
+        void IActivityMonitorClient.OnGroupClosing( IActivityLogGroup group, ref List<ActivityLogGroupConclusion> conclusions )
+        {
+        }
+
+        void IActivityMonitorClient.OnOpenGroup( IActivityLogGroup group )
+        {
+            LogEntry.WriteLog( _writer, true, group.GroupLevel, group.LogTime, group.GroupText, group.GroupTags, group.ExceptionData, group.FileName, group.LineNumber );
+        }
+
+        void IActivityMonitorClient.OnTopicChanged( string newTopic, string fileName, int lineNumber )
+        {
+        }
+
+        void IActivityMonitorClient.OnUnfilteredLog( ActivityMonitorLogData data )
+        {
+            LogEntry.WriteLog( _writer, false, data.Level, data.LogTime, data.Text, data.Tags, data.ExceptionData, data.FileName, data.LineNumber );
+        }
+    }
+}
--- Tests/CK.Monitoring.Tests/AnonymousPipeIntraProcessTests.cs
+++ Tests/CK.Monitoring.Tests/AnonymousPipeIntraProcessTests.cs
@@ -12,6 +12,7 @@ using CK.Monitoring;
 using System.Diagnostics;
 using System.Threading;
 using FluentAssertions;
+using CK.Monitoring.InterProcess;
 
 namespace CK.Monitoring.Tests
 {
@@ -21,54 +22,6 @@ namespace CK.Monitoring.Tests
     [TestFixture]
     public class AnonymousPipeIntraProcessTests
     {
-        class ActivityMonitorAnonymousPipeLogSenderClient : IActivityMonitorClient, IDisposable
-        {
-            readonly CKBinaryWriter _writer;
-            readonly AnonymousPipeClientStream _client;
-
-            public ActivityMonitorAnonymousPipeLogSenderClient( string pipeHandlerName )
-            {
-                _client = new AnonymousPipeClientStream( PipeDirection.Out, pipeHandlerName );
-                _writer = new CKBinaryWriter( _client );
-                _writer.Write( LogReader.CurrentStreamVersion );
-            }
-
-            public void Dispose()
-            {
-                _client.WriteByte( 0 );
-                _client.WaitForPipeDrain();
-                _writer.Dispose();
-                _client.Dispose();
-            }
-
-            void IActivityMonitorClient.OnAutoTagsChanged( CKTrait newTrait )
-            {
-            }
-
-            void IActivityMonitorClient.OnGroupClosed( IActivityLogGroup group, IReadOnlyList<ActivityLogGroupConclusion> conclusions )
-            {
-                LogEntry.WriteCloseGroup( _writer, group.GroupLevel, group.CloseLogTime, conclusions );
-            }
-
-            void IActivityMonitorClient.OnGroupClosing( IActivityLogGroup group, ref List<ActivityLogGroupConclusion> conclusions )
-            {
-            }
-
-            void IActivityMonitorClient.OnOpenGroup( IActivityLogGroup group )
-            {
-                LogEntry.WriteLog( _writer, true, group.GroupLevel, group.LogTime, group.GroupText, group.GroupTags, group.ExceptionData, group.FileName, group.LineNumber );
-            }
-
-            void IActivityMonitorClient.OnTopicChanged( string newTopic, string fileName, int lineNumber )
-            {
-            }
-
-            void IActivityMonitorClient.OnUnfilteredLog( ActivityMonitorLogData data )
-            {
-                LogEntry.WriteLog( _writer, false, data.Level, data.LogTime, data.Text, data.Tags, data.ExceptionData, data.FileName, data.LineNumber );
-            }
-        }
-
         [Test]
         public void sending_log_from_client()
         {
@@ -77,10 +30,10 @@ namespace CK.Monitoring.Tests
             m.Output.RegisterClient( txt );
             using( m.Output.CreateBridgeTo( TestHelper.ConsoleMonitor.Output.BridgeTarget ) )
             {
-                using( var r = LogReceiver.Start( m, false ) )
+                using( var r = SimpleLogPipeReceiver.Start( m, interProcess: false ) )
                 {
                     RunClient( r.PipeName );
-                    r.WaitEnd().Should().Be( LogReceiverEndStatus.Normal );
+                    r.WaitEnd( false ).Should().Be( LogReceiverEndStatus.Normal );
                 }
             }
             var logs = txt.ToString();
@@ -90,97 +43,10 @@ namespace CK.Monitoring.Tests
                          .And.Contain( "Info n°19" );
         }
 
-        public enum LogReceiverEndStatus
-        {
-            None,
-            Normal,
-            MissingEndMarker,
-            Error
-        }
-
-
-        class LogReceiver : IDisposable
-        {
-            readonly AnonymousPipeServerStream _server;
-            readonly CKBinaryReader _reader;
-            readonly IActivityMonitor _monitor;
-            readonly Thread _thread;
-            LogReceiverEndStatus _endFlag;
-
-            LogReceiver( IActivityMonitor m, bool interProcess )
-            {
-                var inherit = interProcess ? HandleInheritability.Inheritable : HandleInheritability.None;
-                _server = new AnonymousPipeServerStream( PipeDirection.In, inherit );
-                _reader = new CKBinaryReader( _server );
-                _monitor = m;
-                PipeName = _server.GetClientHandleAsString();
-                _thread = new Thread( Run );
-                _thread.IsBackground = true;
-                _thread.Start();
-            }
-
-            public string PipeName { get; }
-
-            public LogReceiverEndStatus WaitEnd()
-            {
-                _thread.Join();
-                return _endFlag;
-            }
-
-            public void OnProcessStarted()
-            {
-                _server.DisposeLocalCopyOfClientHandle();
-            }
-
-            public void Dispose()
-            {
-                _thread.Join();
-                _reader.Dispose();
-                _server.Dispose();
-            }
-
-            void Run()
-            {
-                try
-                {
-                    int streamVersion = _reader.ReadInt32();
-                    for( ; ; )
-                    {
-                        var e = LogEntry.Read( _reader, streamVersion, out bool badEndOfStream );
-                        if( e == null || badEndOfStream )
-                        {
-                            _endFlag = badEndOfStream ? LogReceiverEndStatus.MissingEndMarker : LogReceiverEndStatus.Normal;
-                            break;
-                        }
-                        switch( e.LogType )
-                        {
-                            case LogEntryType.Line:
-                                _monitor.UnfilteredLog( e.Tags, e.LogLevel, e.Text, e.LogTime, CKException.CreateFrom( e.Exception ), e.FileName, e.LineNumber );
-                                break;
-                            case LogEntryType.OpenGroup:
-                                _monitor.UnfilteredOpenGroup( e.Tags, e.LogLevel, null, e.Text, e.LogTime, CKException.CreateFrom( e.Exception ), e.FileName, e.LineNumber );
-                                break;
-                            case LogEntryType.CloseGroup:
-                                _monitor.CloseGroup( e.LogTime, e.Conclusions );
-                                break;
-                        }
-                    }
-                }
-                catch( Exception ex )
-                {
-                    _endFlag = LogReceiverEndStatus.Error;
-                    ActivityMonitor.CriticalErrorCollector.Add( ex, "While LogReceiver.Run." );
-                }
-            }
-
-            public static LogReceiver Start( IActivityMonitor m, bool interProcess ) => new LogReceiver( m, interProcess );
-
-        }
-
         void RunClient( string pipeHandlerName )
         {
             var m = new ActivityMonitor( false );
-            using( var pipe = new ActivityMonitorAnonymousPipeLogSenderClient( pipeHandlerName ) )
+            using( var pipe = new SimplePipeSenderActivityMonitorClient( pipeHandlerName ) )
             {
                 m.Output.RegisterClient( pipe );
                 using( m.OpenInfo( "From client." ) )
