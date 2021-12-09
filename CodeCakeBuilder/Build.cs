fe0819a26c12336e16cbdb0491577a2749d650ad
Olivier Spinelli <olivier.spinelli@signature-code.com>
2021-12-09 18:21:16 +0100

    Udated CCB.

--- CodeCakeBuilder/Build.cs
+++ CodeCakeBuilder/Build.cs
@@ -1,14 +1,9 @@
 using Cake.Common.IO;
 using Cake.Core;
-
 using Cake.Core.Diagnostics;
-using SimpleGitVersion;
-
-
 
 namespace CodeCake
 {
-    
     public partial class Build : CodeCakeHost
     {
         public Build()
